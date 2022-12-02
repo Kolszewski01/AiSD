9132d0017f0fd2fbeb287b6ed680a685a0c7c5c9
Konrad Olszewski <92027283+Kolszewski01@users.noreply.github.com>
2022-12-02 10:50:45 +0100

    Update binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -1,5 +1,5 @@
-from typing import List, Any, Callable, Union
-# from graphviz import *
+from typing import Any, Callable
+from graphviz import *
 
 class BinaryNode:
     value: Any
@@ -8,9 +8,8 @@ class BinaryNode:
 
     def __init__(self, value) -> None:
         self.value=value
-        self.left_child=None
-        self.right_child=None
-
+        self.left_child = None
+        self.right_child = None
 
     def is_leaf(self) -> bool:
         if self.left_child or self.right_child:
@@ -19,12 +18,10 @@ class BinaryNode:
 
 
     def add_left(self, value: Any) -> None:
-
-        self.left_child = BinaryNode(value)
+        self.left_child=BinaryNode(value)
 
     def add_right(self, value: Any) -> None:
-
-        self.right_child = BinaryNode(value)
+        self.right_child=BinaryNode(value)
 
     def traverse_in_order(self, visit: Callable[[Any], None]) -> None:
         if self.left_child:
@@ -35,45 +32,28 @@ class BinaryNode:
 
     def traverse_post_order(self, visit: Callable[[Any], None]) -> None:
         if self.left_child:
-            self.left_child.traverse_in_order(visit)
+            self.left_child.traverse_post_order(visit)
         if self.right_child:
-            self.right_child.traverse_in_order(visit)
+            self.right_child.traverse_post_order(visit)
         visit(self)
 
-    def traverse_post_order(self, visit: Callable[[Any], None]) -> None:
+    def traverse_pre_order(self, visit: Callable[[Any], None]) -> None:
         visit(self)
         if self.left_child:
-            self.left_child.traverse_in_order(visit)
+            self.left_child.traverse_pre_order(visit)
         if self.right_child:
-            self.right_chil.traverse_in_order(visit)
+            self.right_child.traverse_pre_order(visit)
 
 
+    def __str__(self) -> str:
+        return str(self.value)
 
 
-    # def search(self,value: Any) -> Union["TreeNode", None]:
-    #     if self.value==value:
-    #         return self
-    #     for x in self.children:
-    #         wynik=x.search(value)
-    #         if wynik:
-    #             return wynik
-
-    # def show(self, x=Graph('NTGAWTP')):
-    #     x.node(str(self), str(self.value), shape='egg',color="blue")
-    #     for i in self.children:
-    #         x.edge(str(self), str(i),color="red")
-    #         i.show(x)
-    #     return x
-    #
-#
 class BinaryTree:
     root: BinaryNode
 
-    def __init__(self, root) -> None:
-        self.root = root
-
-    def add(self, value: Any, parent_name: Any) -> None:
-        parent_name.children.append(BinaryNode(value))
+    def __init__(self, root: BinaryNode) -> None:
+        self.root=root
 
     def traverse_in_order(self, visit: Callable[[Any], None]) -> None:
         self.root.traverse_in_order(visit)
@@ -84,28 +64,26 @@ class BinaryTree:
     def traverse_pre_order(self, visit: Callable[[Any], None]) -> None:
         self.root.traverse_pre_order(visit)
 
-    def show(self) -> None:
-        self.root.show().render(filename='Obraz', format='jpg')
 
+tree = BinaryNode(10)
+tree.add_left(9)
+tree.left_child.add_left(1)
+tree.left_child.add_right(3)
+tree.add_right(2)
+tree.right_child.add_left(4)
+tree.right_child.add_right(6)
 
+treen=BinaryTree(tree)
 
-def rysuj(x: 'BinaryNode') -> None:
-    if isinstance(x, BinaryNode):
-        print(x.value)
-    else:
-        print(x)
+treen.traverse_in_order(print)
+print('----------------')
+treen.traverse_post_order(print)
+print('----------------')
+treen.traverse_pre_order(print)
 
-
-
-b=BinaryNode(10)
-b.add_left(9)
-b.add_right(2)
-b.left_child.add_left(1)
-b.left_child.add_right(3)
-b.right_child.add_right(6)
-b.right_child.add_left(4)
-
-tree = BinaryTree(b)
-
-tree.traverse_in_order(print)
+assert treen.root.value == 10
+assert treen.root.right_child.value == 2
+assert treen.root.right_child.is_leaf() is False
+assert treen.root.left_child.left_child.value == 1
+assert treen.root.left_child.left_child.is_leaf() is True
 
